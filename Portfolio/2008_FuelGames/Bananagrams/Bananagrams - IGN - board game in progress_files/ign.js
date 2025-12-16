35ad5fe569d02527679f0f51529875ca5d6dcdcc
Anomy <allison.allain@gmail.com>
2025-12-16 12:33:36 -0500

    portfolio screenshots and video

---
+++ Portfolio/2008_FuelGames/Bananagrams/Bananagrams - IGN - board game in progress_files/ign.js
@@ -0,0 +1,17 @@
+
+(function() {
+    var domain = '.ign.com'; var cc = 'US'; var akcc = 'US';
+    if (cc && document.cookie.indexOf('geoCC') == -1) {
+      document.cookie='geoCC='+cc+';path=/;domain='+domain;
+      document.cookie='geoCC='+cc+';path=/;domain=.ziffdavisinternational.com';
+    }
+})();
+    
+(function() {
+var d = document.createElement('div'); d.style.position='absolute'; d.style.bottom='0px'; d.style.left='0px';
+var dd = document.createElement('div'); dd.id="pgxuie48a"; dd.style.width="10px"; dd.style.height="10px"; dd.classList.add("ad-top-728");
+d.appendChild(dd); document.body.appendChild(d); if (d.clientWidth && d.clientHeight) { d.parentNode.removeChild(d); return; }
+var s=document.createElement('script'); s.setAttribute("type","text/javascript"); s.setAttribute("src", "https://pg.ignimgs.com/ign.js");
+document.getElementsByTagName("head")[0].appendChild(s);
+})();
+
